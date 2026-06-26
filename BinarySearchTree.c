fe8b6bc95244dc4ffe74eb1167f449e3377bbf5b
1leven <3176754559@qq.com>
2026-06-26 22:02:01 +0800

    recursive algorithm

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -9,7 +9,7 @@ typedef struct BinarySearchTree
 }BST;
 
 //1、根节点为全局变量
-BST* root=NULL;
+// BST* root=NULL;
 //GetNewNode
 BST* GetNewNode(int data){
     BST* newNode=(BST*)malloc(sizeof(BST));
@@ -33,34 +33,61 @@ BST* GetNewNode(int data){
 // }
 //双重指针
 void Insert(BST** rootPtr,int data){
+    if(*rootPtr==NULL){
+        *rootPtr=GetNewNode(data);
+        return;
+    }
+    // while(*rootPtr!=NULL){
+    //    if(data<=(*rootPtr)->data){
+    //         rootPtr=&((*rootPtr)->left);
+    //    }else if(data>(*rootPtr)->data){
+    //         rootPtr=&(*rootPtr)->right;
+    //    }
+    // }
+    // *rootPtr=GetNewNode(data);
+    if(data<=(*rootPtr)->data){
+        Insert(&((*rootPtr)->left),data);
+    }
+    else{
+        Insert(&((*rootPtr)->right),data);
+    }
+    //*rootPtr=GetNewNode(data);
+
 
 }
 bool Search(BST* root,int x){
     BST* temp=root;
     //普通版
-    // while(temp!=NULL){
-    //     if(temp->data==x)
-    //         return true;
-    //     else if(x<temp->data){
-    //         temp=temp->left;
-    //     }else{
-    //         temp=temp->right;
-    //     }
-    // }
-    //return false;
+    while(temp!=NULL){
+        if(temp->data==x)
+            return true;
+        else if(x<temp->data){
+            temp=temp->left;
+        }else{
+            temp=temp->right;
+        }
+    }
+    return false;
     
     //递归版
 
 }
 int main(){
+    BST* root=NULL;
+    BST** rootPtr=&root;
+    Insert(rootPtr,1);
+    Insert(rootPtr,2);
+    Insert(rootPtr,5);
+    Insert(rootPtr,3);
     // root=Insert(root,1);
     // root=Insert(root,2);
     // root=Insert(root,5);
     // root=Insert(root,8);
     // root=Insert(root,6);
-    // if(Search(root,7)){
-    //     printf("find");
-    // }else{
-    //     printf("wrong");
-    // }
+    if(Search(root,5)){
+        printf("find");
+    }else{
+        printf("wrong");
+    }
+
 }
